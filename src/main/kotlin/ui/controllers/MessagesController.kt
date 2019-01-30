bebef32cd80db6b80a3ee0a42cd0cd146a550013
Kevin <spearskw@gmail.com>
2019-01-30 20:37:22 +1100

    Removed search (for now)
    The MessagePane view is getting out of control, and search was
    a small part of it. I need to revamp search anyway, so it is the
    first casualty in what will be an attempt to wrangle the mess
    that the UI code is becoming.
    
    p.s. I think the reason it is a mess is because I still don't
    understand how to use a ListCellFragment

--- src/main/kotlin/ui/controllers/MessagesController.kt
+++ src/main/kotlin/ui/controllers/MessagesController.kt
@@ -1,35 +1,16 @@
 package ui.controllers
 
 import adaptors.fetchFromDB
-import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleStringProperty
-import javafx.scene.input.KeyCode
-import javafx.scene.input.KeyCodeCombination
-import javafx.scene.input.KeyCombination
-import javafx.scene.input.KeyEvent
-import tornadofx.Controller
-import tornadofx.EventBus
-import tornadofx.FXEvent
-import tornadofx.observable
 import model.Message
+import tornadofx.Controller
+import tornadofx.observable
 
 class MessagesController : Controller() {
 
-    val searchTerm = SimpleStringProperty()
-    val isSearchVisible = SimpleBooleanProperty()
     val messageList = mutableListOf<Message>().observable()
     val settings: SettingsController by inject()
     val chatName = SimpleStringProperty()
-
-    fun handleKeyPress(keyEvent: KeyEvent) {
-        val findMac = KeyCodeCombination(KeyCode.F, KeyCombination.META_DOWN)
-        if (findMac.match(keyEvent)) {
-            isSearchVisible.value = true
-            fire(SearchOpenedEvent())
-        } else if (keyEvent.code == KeyCode.ESCAPE) {
-            isSearchVisible.value = false
-        }
-    }
 
     fun loadChat(chatID: Int) {
         chatName.value = chatID.toString()
@@ -41,6 +22,3 @@ class MessagesController : Controller() {
         }
     }
 }
-
-// TODO I don't know where to put this
-class SearchOpenedEvent : FXEvent(EventBus.RunOn.BackgroundThread)
--- src/main/kotlin/ui/views/MessagePane.kt
+++ src/main/kotlin/ui/views/MessagePane.kt
@@ -1,11 +1,9 @@
 package ui.views
 
-import javafx.application.Platform
 import javafx.geometry.Pos
 import javafx.scene.layout.Priority
 import tornadofx.*
 import ui.controllers.MessagesController
-import ui.controllers.SearchOpenedEvent
 import ui.styles.MessageStyle
 import utils.getImage
 
@@ -13,7 +11,6 @@ import utils.getImage
 class MessagePane : View("Messages") {
 
     private val controller: MessagesController by inject()
-    private val filteredMessages = SortedFilteredList(controller.messageList)
 
     override val root = vbox {
         hbox {
@@ -25,7 +22,7 @@ class MessagePane : View("Messages") {
             }
             alignment = Pos.CENTER
         }
-        listview(filteredMessages) {
+        listview(controller.messageList) {
             vgrow = Priority.ALWAYS
             cellFormat {
                 graphic = vbox {
@@ -54,21 +51,6 @@ class MessagePane : View("Messages") {
                 }
 
             }
-        }
-
-        textfield(controller.searchTerm) {
-            subscribe<SearchOpenedEvent> {
-                Platform.runLater { requestFocus() }
-            }
-            filteredMessages.filterWhen(textProperty()) { query, item ->
-                item.text?.contains(query, true) ?: false
-            }
-            managedProperty().bind(visibleProperty())
-            visibleWhen(controller.isSearchVisible)
-        }
-
-        setOnKeyPressed {
-            controller.handleKeyPress(it)
         }
 
         paddingAll = 10.0
