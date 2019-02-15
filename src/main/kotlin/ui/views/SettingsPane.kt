3b57349d8d075eeacccc7bbe928665c63821b74a
Kevin <spearskw@gmail.com>
2019-02-15 13:02:11 +1100

    Added default values for settings

--- src/main/kotlin/ui/views/SettingsPane.kt
+++ src/main/kotlin/ui/views/SettingsPane.kt
@@ -5,11 +5,17 @@ import tornadofx.*
 import ui.controllers.ConversationController
 import ui.viewmodels.SettingsViewModel
 import ui.viewmodels.validateDB
+import java.io.File
 
 class SettingsPane : View("Settings") {
 
     private val model: SettingsViewModel by inject()
     private val controller: ConversationController by inject()
+
+    init {
+        model.messageDB.value = "${System.getProperty("user.home")}/Library/Messages/chat.db"
+        model.addressBookDB.value = findAddressBook()
+    }
 
     override val root = form {
         prefWidth = 400.0
@@ -46,4 +52,16 @@ class SettingsPane : View("Settings") {
             }
         }
     }
+}
+
+fun findAddressBook(): String? {
+    val rootDir = "${System.getProperty("user.home")}/Library/Application Support/AddressBook/Sources"
+
+    //For now we'll assume the biggest address book is the one we want.
+    //In the future we'd like to merge all available address books
+    val db = File(rootDir).walk()
+        .filter { it.toString().endsWith("db") }
+        .maxBy { it.length() }
+
+    return db?.path
 }
